907aa677582dc17ec821f9ba4262ee96355b37e9
Zhl1102 <1533938229@qq.com>
2022-02-17 17:48:36 +0800

    resume

--- first_app.py
+++ first_app.py
@@ -3,8 +3,4 @@ import numpy as np
 import pandas as pd
 import time
 
-@st.cache
-
-def my_slow_funcation(arg1, arg2):
-
-		return arg1
+st.title("个人简历")
--- second_app.py
+++ second_app.py
@@ -3,15 +3,14 @@ import numpy
 import pandas
 import time
 
-'Starting a long computation...'
-
-# Add a placeholder
-header_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-		header_iteration.text(f'Iteration {i + 1}')
-		bar.progress(i + 1)
-		time.sleep(0.1)
-
-'...and now we\'re done!'
+st.set_page_config(
+     page_title="Ex-stream-ly Cool App",
+     page_icon="🧊",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.extremelycoolapp.com/help',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+)
